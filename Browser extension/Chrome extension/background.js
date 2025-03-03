fa81f76957def3a0f59cf38350104d092cc9037e
aStereoID <43568056+aStereoID@users.noreply.github.com>
2025-03-03 11:55:40 +0100

    Update background.js

--- Browser extension/Chrome extension/background.js
+++ Browser extension/Chrome extension/background.js
@@ -1,11 +1,3 @@
 chrome.runtime.onInstalled.addListener(() => {
     console.log("Serbski Text-to-Speech Erweiterung installiert.");
 });
-
-// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    // if (message.action === "updateStatus") {
-        // console.log("TTS Status aktualisiert:", message.playing ? "Abspielen" : "Pause");
-    // } else if (message.action === "updateMute") {
-        // console.log("TTS Mute-Status aktualisiert:", message.muted ? "Stummgeschaltet" : "Nicht stummgeschaltet");
-    // }
-// });
